203c408dc94c6c2a9ae72e72225eebcd9970f1e7
syouzan420 <syouzanponkikki@yahoo.co.jp>
2024-03-03 22:23:37 +0900

    update

--- src/Code.hs
+++ src/Code.hs
@@ -13,12 +13,14 @@ import Definition
 
 makeLenses ''Game
 
-exeCode :: T.Text -> EventM Name Game () 
+type StateG = EventM Name Game ()
+
+exeCode :: T.Text -> StateG 
 exeCode evt = do 
   let etxs = T.split (==' ') evt
   mapM_ exeOneCode etxs
 
-exeOneCode :: T.Text -> EventM Name Game () 
+exeOneCode :: T.Text -> StateG 
 exeOneCode evt = do
   let en_ags = T.split (=='_') evt
   let (en,ags) = fromMaybe ("null",[]) (uncons en_ags)
@@ -29,10 +31,10 @@ exeOneCode evt = do
     "pls" -> setPlayer
     _ -> return ()
 
-setPlayer :: EventM Name Game ()
+setPlayer :: StateG 
 setPlayer = ipl .= True
 
-moveDialog :: T.Text -> EventM Name Game () 
+moveDialog :: T.Text -> StateG 
 moveDialog title = do
   textSections <- use txs
   let tsKeyValues = map (\(TS ti tx) -> (ti,tx)) textSections
